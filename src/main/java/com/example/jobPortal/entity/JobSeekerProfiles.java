1202da0be104792e31d4ab60758bd77ead631ef5
Rajeev Dutta <aayushisinha690@gmail.com>
2025-07-02 12:40:36 +0530

    adding files

---
+++ src/main/java/com/example/jobPortal/entity/JobSeekerProfiles.java
@@ -0,0 +1,9 @@
+package com.example.jobPortal.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "job_seeker_profile")
+public class JobSeekerProfiles {
+}
